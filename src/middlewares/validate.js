5087c9546bdc656ca5a5a48747a470ff5b9bcb4b
Suleyman <suleyman@suleyman-popos.local>
2021-12-26 23:36:10 +0300

    Fix the message

--- src/middlewares/validate.js
+++ src/middlewares/validate.js
@@ -3,7 +3,7 @@ const validate = (schema) => (req, res, next) => {
 
   if (error) {
     const errorMessage = error?.details?.map((detail) => detail?.message).join(', ');
-    return res.status(400).send({ error: errorMessage });
+    return res.status(400).send({ code: 1, msg: errorMessage });
   }
 
   Object.assign(req, value);
